e0853d77c517c3c18d838d9da0e2bc5afc03efad
James Riley <jamriley@cisco.com>
2019-04-30 11:52:21 +0100

    working flask webpage with python integration

--- common.py
+++ common.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import * 
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    flash('test')
     return render_template('home.html')
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
 
